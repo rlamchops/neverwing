413436bd3e835a87350c6890e1be184ba78bd488
Raymond Lam <lamycraymond@hotmail.com>
2017-05-22 23:03:50 -0400

    basic premise achieved, however scrolling up in the chat window to view previous messages crashes messenger

---
+++ README.md
@@ -0,0 +1,1 @@
+Goal: Remove everwing messages from messenger
---
+++ neverwing/remove_script.js
@@ -0,0 +1,28 @@
+/*document.addEventListener("DOMContentLoaded", function(event) {
+  var list = document.querySelectorAll("div._497p._11es");
+  while(list[0]) {
+    list[0].parentNode.removeChild(list[0]);
+  }
+});
+*/
+/*document.addEventListener("DOMContentLoaded", function(event) {
+  var myScript = document.createElement('script');
+  myScript.textContent = actualCode;
+  (document.head||document.documentElement).appendChild(myScript);
+  myScript.parentNode.removeChild(myScript);
+});*/
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript({
+    code: `
+      var list = Array.prototype.slice.call(document.querySelectorAll("div._497p._11es"));
+      while(list[0]) {
+        console.log(list[0].innerHTML);
+        list[0].parentNode.removeChild(list[0]);
+        list.shift();
+        if (!list[0]) {break;}
+        console.log(list[0].innerHTML);
+      }
+    `
+  });
+});
